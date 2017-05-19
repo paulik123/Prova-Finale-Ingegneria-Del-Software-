9e2065be589ac368a4bc4741515665a5d37f92cc
Ene Paul <paulalexandru.ene@mail.polimi.it>
2017-05-19 18:53:43 +0200

    finished adding all actions to actionbuilder (i hope)

--- src/main/java/it/polimi/ingsw/ps45/model/actions/ActionBuilder.java
+++ src/main/java/it/polimi/ingsw/ps45/model/actions/ActionBuilder.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.ps45.model.actions;
 
+import it.polimi.ingsw.ps45.model.actions.CouncilPrivilege.CouncilPrivilege;
 import it.polimi.ingsw.ps45.model.actions.state.*;
 import it.polimi.ingsw.ps45.model.area.Area;
 import it.polimi.ingsw.ps45.model.area.Board;
@@ -93,6 +94,7 @@ public class ActionBuilder {
 		Pawn pawn = p.getResourceSet().getPawn(pt);
 		int pawnValue = calculatePawnValue(pt, servantsAdded);
 		ConsumableSet servantsSet = servantsToConsumableSet(servantsAdded);
+		servantsSet.makeDiscount(p.getResourceSet().getTerritoryActionDiscount());
 		
 		if(canPlacePawnTerritoryRun(pawn, pawnValue, area, servantsSet)){
 			makePlayerPay(servantsSet, pawn);
@@ -107,6 +109,7 @@ public class ActionBuilder {
 		int pawnValue = calculatePawnValue(pt, servantsAdded);
 		ConsumableSet cost = area.getCharacter().cost();
 		cost.collect(servantsToConsumableSet(servantsAdded));
+		cost.makeDiscount(p.getResourceSet().getCharacterActionDiscount());
 		
 		if(canPlacePawnCharacterRun(pawn, pawnValue, area, cost)){
 			makePlayerPay(cost, pawn);
@@ -121,6 +124,7 @@ public class ActionBuilder {
 		int pawnValue = calculatePawnValue(pt, servantsAdded);
 		ConsumableSet cost = area.getBuilding().cost();
 		cost.collect(servantsToConsumableSet(servantsAdded));
+		cost.makeDiscount(p.getResourceSet().getBuildingActionDiscount());
 		
 		if(canPlacePawnBuildingRun(pawn, pawnValue, area, cost)){
 			makePlayerPay(cost, pawn);
@@ -139,6 +143,7 @@ public class ActionBuilder {
 		else cost = area.getVenture().costII();
 		
 		cost.collect(servantsToConsumableSet(servantsAdded));
+		cost.makeDiscount(p.getResourceSet().getVentureActionDiscount());
 		
 		if(canPlacePawnVentureRun(pawn, pawnValue, area, cost)){
 			makePlayerPay(cost, pawn);
@@ -147,12 +152,123 @@ public class ActionBuilder {
 
 	}
 	
+	public void harvest() throws Exception{
+		if(!state.harvestAction()) throw new Exception("Action not allowed - state is false");
+		new HarvestAction(p, state.actionValue()).run();
+	}
+	
+	public void production(ProductionMode[] pm) throws Exception{
+		if(!state.productionAction()) throw new Exception("Action not allowed - state is false");
+		new ProductionAction(p, pm, state.actionValue()).run();
+	}
+	
+	public void NoPawnTerritory(TerritoryCardArea area, int servantsAdded) throws Exception{
+		if(!state.takeTerritoryAction()) throw new Exception("Action not allowed - state is false");
+		int value = calculateValue(state.actionValue(), servantsAdded);
+		ConsumableSet servantsSet = servantsToConsumableSet(servantsAdded);
+		servantsSet.makeDiscount(p.getResourceSet().getTerritoryActionDiscount());
+		
+		if(canNoPawnTerritoryRun(value, area, servantsSet)){
+			makePlayerPay(servantsSet);
+			new NoPawnTerritoryAction(p, area).run();
+		}else throw new Exception("Action not allowed - pawn/consumables unavailable");
+	}
+	
+	public void NoPawnCharacter(CharacterCardArea area, int servantsAdded) throws Exception{
+		if(!state.takeTerritoryAction()) throw new Exception("Action not allowed - state is false");
+		int value = calculateValue(state.actionValue(), servantsAdded);
+		ConsumableSet cost = area.getCharacter().cost();
+		cost.collect(servantsToConsumableSet(servantsAdded));
+		cost.makeDiscount(p.getResourceSet().getCharacterActionDiscount());
+		
+		if(canNoPawnCharacterRun(value, area, cost)){
+			makePlayerPay(cost);
+			new NoPawnCharacterAction(p, area).run();
+		}else throw new Exception("Action not allowed - pawn/consumables unavailable");
+	}
+	
+	public void NoPawnBuilding(BuildingCardArea area, int servantsAdded) throws Exception{
+		if(!state.takeBuildingAction()) throw new Exception("Action not allowed - state is false");
+		int value = calculateValue(state.actionValue(), servantsAdded);
+		ConsumableSet cost = area.getBuilding().cost();
+		cost.collect(servantsToConsumableSet(servantsAdded));
+		cost.makeDiscount(p.getResourceSet().getBuildingActionDiscount());
+		
+		if(canNoPawnBuildingRun(value, area, cost)){
+			makePlayerPay(cost);
+			new NoPawnBuildingAction(p, area).run();
+		}else throw new Exception("Action not allowed - pawn/consumables unavailable");
+	}
+	
+	public void NoPawnVenture(VentureCardArea area, int servantsAdded, VentureMode mode) throws Exception{
+		if(!state.takeVentureAction()) throw new Exception("Action not allowed - state is false");
+		int value = calculateValue(state.actionValue(), servantsAdded);
+		
+		ConsumableSet cost;
+		if(mode == VentureMode.FIRST) cost = area.getVenture().costI();
+		else cost = area.getVenture().costII();
+		
+		cost.collect(servantsToConsumableSet(servantsAdded));
+		cost.makeDiscount(p.getResourceSet().getVentureActionDiscount());
+		
+		if(canNoPawnVentureRun(value, area, cost)){
+			makePlayerPay(cost);
+			new NoPawnVentureAction(p, area).run();
+		}else throw new Exception("Action not allowed - pawn/consumables unavailable");
+	}
+	
+	public void exchangeCouncilPrivilegeOne(CouncilPrivilege cp1) throws Exception{
+		if(!state.coucilPrivilegeActionOne())throw new Exception("Action not allowed - state is false");
+		new CouncilPrivilegeOneAction(p, cp1).run();
+		setState(new NoActionState());
+	}
+	
+	public void exchangeCouncilPrivilegeTwo(CouncilPrivilege cp1, CouncilPrivilege cp2) throws Exception{
+		if(!state.coucilPrivilegeActionTwo())throw new Exception("Action not allowed - state is false");
+		if(CouncilPrivilege.areDifferent(cp1, cp2)) new CouncilPrivilegeTwoAction(p, cp1, cp2).run();
+		else throw new Exception("Council privileges are not different");
+		setState(new NoActionState());
+	}
+	
+	public void exchangeCouncilPrivilegeThree(CouncilPrivilege cp1, CouncilPrivilege cp2, CouncilPrivilege cp3) throws Exception{
+		if(!state.coucilPrivilegeActionThree())throw new Exception("Action not allowed - state is false");
+		
+		if(CouncilPrivilege.areDifferent(cp1, cp2, cp3)) new CouncilPrivilegeThreeAction(p, cp1, cp2, cp3).run();
+		else throw new Exception("Council privileges are not different");
+		setState(new NoActionState());
+	}
 
 	
 	
 	
 	
 	
+	
+	
+	private boolean canNoPawnVentureRun(int value, VentureCardArea area, ConsumableSet cost){
+		return !board.getVentureTower().isOccupiedByPlayer(p) &&
+				p.getResourceSet().getCharacterList().size() < 6 &&
+				hasGeneralRequirementsNoPawn(value, area, cost);
+	}
+	
+	private boolean canNoPawnBuildingRun(int value, BuildingCardArea area, ConsumableSet cost){
+		return !board.getBuildingTower().isOccupiedByPlayer(p) &&
+				p.getResourceSet().getCharacterList().size() < 6 &&
+				hasGeneralRequirementsNoPawn(value, area, cost);
+	}
+	
+	private boolean canNoPawnCharacterRun(int value, CharacterCardArea area, ConsumableSet cost){
+		return !board.getCharacterTower().isOccupiedByPlayer(p) &&
+				p.getResourceSet().getCharacterList().size() < 6 &&
+				hasGeneralRequirementsNoPawn(value, area, cost);
+	}
+	
+	private boolean canNoPawnTerritoryRun(int value, TerritoryCardArea area, ConsumableSet cost){
+		return !board.getTerritoryTower().isOccupiedByPlayer(p) &&
+				hasMilitaryForTerritory() &&
+				p.getResourceSet().getTerritoryList().size() < 6 &&
+				hasGeneralRequirementsNoPawn(value, area, cost);
+	}
 	
 	private boolean canPlacePawnVentureRun(Pawn pawn, int pawnValue, VentureCardArea area, ConsumableSet cost){
 		return !board.getVentureTower().isOccupiedByPlayer(p) &&
@@ -201,6 +317,12 @@ public class ActionBuilder {
 				   	p.getResourceSet().hasConsumables(cost);
 	}
 	
+	private boolean hasGeneralRequirementsNoPawn(int value, Area area, ConsumableSet cost){
+		   return   area.isAvailable() &&
+				   	value > area.getCost() && 
+				   	p.getResourceSet().hasConsumables(cost);
+	}
+	
 	private boolean hasMilitaryForTerritory(){
 		int size = p.getResourceSet().getTerritoryList().size() + 1;
 		int militaryPoints = p.getResourceSet().getResources().getMilitaryPoins();
@@ -216,12 +338,21 @@ public class ActionBuilder {
 		p.getResourceSet().pay(cs, pawn);
 	}
 	
+	private void makePlayerPay(ConsumableSet cs){
+		p.getResourceSet().getResources().pay(cs);
+	}
+	
 	private int calculatePawnValue(PawnType pt, int servantsAdded){
 		int pawnValue = p.getResourceSet().getPawnValue(pt);
 		
 		//Checking if user has to add 2 servants to increase value by one. Odd #s are rounded down.
 		if(p.getResourceSet().getPermanentEffects().isAddingServantsPenalty())return pawnValue  + (int) ((double)servantsAdded * 0.5);
 		else return pawnValue + servantsAdded;
+	}
+	
+	private int calculateValue(int value, int servantsAdded){
+		if(p.getResourceSet().getPermanentEffects().isAddingServantsPenalty())return value  + (int) ((double)servantsAdded * 0.5);
+		else return value + servantsAdded;
 	}
 	
 	private ConsumableSet servantsToConsumableSet(int servantsAdded){
