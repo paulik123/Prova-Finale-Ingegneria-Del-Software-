2a9576d938778c32e92d0b7dc7db19b512e71e52
ichimiurii <iurie.ichim@gmail.com>
2017-05-22 15:58:17 +0200

    SetUp functions to serialise areas in cars

--- src/main/java/it/polimi/ingsw/ps45/SerializeCardArea.java
+++ src/main/java/it/polimi/ingsw/ps45/SerializeCardArea.java
@@ -2,57 +2,76 @@ package it.polimi.ingsw.ps45;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Writer;
+import java.util.ArrayList;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import it.polimi.ingsw.ps45.model.area.Board;
 import it.polimi.ingsw.ps45.model.area.NoCardArea;
+import it.polimi.ingsw.ps45.model.area.PlayerPawnPair;
 import it.polimi.ingsw.ps45.model.area.cardarea.BuildingCardArea;
 import it.polimi.ingsw.ps45.model.area.cardarea.CharacterCardArea;
 import it.polimi.ingsw.ps45.model.area.cardarea.TerritoryCardArea;
 import it.polimi.ingsw.ps45.model.area.cardarea.VentureCardArea;
+import it.polimi.ingsw.ps45.model.cards.Era;
+import it.polimi.ingsw.ps45.model.cards.Territory;
+import it.polimi.ingsw.ps45.model.cards.Venture;
 import it.polimi.ingsw.ps45.model.effects.CollectEffect;
+import it.polimi.ingsw.ps45.model.effects.CouncilPrivilegeOneAndCollectEffect;
 import it.polimi.ingsw.ps45.model.effects.CouncilPrivilegeTwoEffect;
+import it.polimi.ingsw.ps45.model.effects.Effect;
+import it.polimi.ingsw.ps45.model.effects.HarvestEffect;
 import it.polimi.ingsw.ps45.model.effects.NoEffect;
+import it.polimi.ingsw.ps45.model.effects.ProductionEffect;
 import it.polimi.ingsw.ps45.model.player.ConsumableSet;
+import it.polimi.ingsw.ps45.model.player.Player;
 
 public class SerializeCardArea {
-public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		
-		ConsumableSet cs = new ConsumableSet();
-
-		/*
-		CollectEffect effect = new CollectEffect(cs);
-		cs.setCoins(2);
+		ConsumableSet effectConsumableSet = new ConsumableSet();
+		effectConsumableSet.setWood(0);
+		effectConsumableSet.setStone(0);
+		effectConsumableSet.setCoins(2);
+		effectConsumableSet.setMilitaryPoins(0);
+		effectConsumableSet.setServants(0);
+		effectConsumableSet.setFaithPoints(0);
+		effectConsumableSet.setVictoryPoints(0);
+		
+		String path = "C://outout//json//VentureCardAreaThirdFloor.json";
+		
+		CollectEffect collectEffect = new CollectEffect(effectConsumableSet);
+		//CouncilPrivilegeOneEffect e = new CouncilPrivilegeOneEffect();
+		//HarvestEffect harvestEffect = new HarvestEffect(3);
+		
+		//CouncilPrivilegeOneAndCollectEffect e = new CouncilPrivilegeOneAndCollectEffect(effectConsumableSet);
 		NoEffect noEffect = new NoEffect();
-    	VentureCardArea area = new VentureCardArea(7, effect);
-		try {
-	         FileOutputStream fileOut = new FileOutputStream("C://outout//VentureCardAreaThirdFloor.ser");
-	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
-	         out.writeObject(area);
-	         out.close();
-	         fileOut.close();
-	      }catch(IOException i) {
-	         i.printStackTrace();
-	      }
-	      */
-		System.out.println(loadFromFile("VentureCardAreaThirdFloor.ser").getCost());
+		
+    	VentureCardArea area = new VentureCardArea(7, collectEffect);
+
+	      
+        Writer writer = new FileWriter(path);
+
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Effect.class,
+                        new PropertyBasedInterfaceMarshal()).create();
+        gson.toJson(area, writer);
+
+        writer.close();
+        
+        
+        
+        
+        VentureCardArea aaa = gson.fromJson(new FileReader(path), VentureCardArea.class);
+        System.out.println(aaa.getCost());
+
 
 	}
-
-public static VentureCardArea loadFromFile(String name){
-	VentureCardArea x = null;
-    try {
-         FileInputStream fileIn = new FileInputStream("serialized//areas//" + name);
-         ObjectInputStream in = new ObjectInputStream(fileIn);
-         x = (VentureCardArea) in.readObject();
-         in.close();
-         fileIn.close();
-      }catch(IOException i) {
-         i.printStackTrace();
-      }catch(Exception ex) {
-         ex.printStackTrace();
-      }
-    return x;
-}
 }
--- src/main/java/it/polimi/ingsw/ps45/SerializeNoCardArea.java
+++ src/main/java/it/polimi/ingsw/ps45/SerializeNoCardArea.java
@@ -2,57 +2,70 @@ package it.polimi.ingsw.ps45;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Writer;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import it.polimi.ingsw.ps45.model.area.Board;
 import it.polimi.ingsw.ps45.model.area.NoCardArea;
+import it.polimi.ingsw.ps45.model.area.PlayerPawnPair;
 import it.polimi.ingsw.ps45.model.cards.Era;
 import it.polimi.ingsw.ps45.model.cards.Territory;
+import it.polimi.ingsw.ps45.model.cards.Venture;
 import it.polimi.ingsw.ps45.model.effects.CollectEffect;
 import it.polimi.ingsw.ps45.model.effects.CouncilPrivilegeOneAndCollectEffect;
 import it.polimi.ingsw.ps45.model.effects.CouncilPrivilegeTwoEffect;
+import it.polimi.ingsw.ps45.model.effects.Effect;
 import it.polimi.ingsw.ps45.model.effects.HarvestEffect;
 import it.polimi.ingsw.ps45.model.effects.ProductionEffect;
 import it.polimi.ingsw.ps45.model.player.ConsumableSet;
 import it.polimi.ingsw.ps45.model.player.Player;
 
 public class SerializeNoCardArea {
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		
-		ConsumableSet cs = new ConsumableSet();
-		cs.setCoins(1);
-		CouncilPrivilegeTwoEffect e = new CouncilPrivilegeTwoEffect();
-    	NoCardArea area = new NoCardArea(1,1, e);
-		try {
-	         FileOutputStream fileOut = new FileOutputStream("C://outout//.ser");
-	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
-	         out.writeObject(area);
-	         out.close();
-	         fileOut.close();
-	      }catch(IOException i) {
-	         i.printStackTrace();
-	      }
+		ConsumableSet effectConsumableSet = new ConsumableSet();
+		effectConsumableSet.setWood(0);
+		effectConsumableSet.setStone(0);
+		effectConsumableSet.setCoins(1);
+		effectConsumableSet.setMilitaryPoins(0);
+		effectConsumableSet.setServants(0);
+		effectConsumableSet.setFaithPoints(0);
+		effectConsumableSet.setVictoryPoints(0);
+		
+		String path = "C://outout//json//HarvestBigArea.json";
+		
+		//CollectEffect collectEffect = new CollectEffect(effectConsumableSet);
+		//CouncilPrivilegeOneEffect e = new CouncilPrivilegeOneEffect();
+		HarvestEffect harvestEffect = new HarvestEffect(3);
+		
+		//CouncilPrivilegeOneAndCollectEffect e = new CouncilPrivilegeOneAndCollectEffect(effectConsumableSet);
+		
+    	NoCardArea area = new NoCardArea(1,999, harvestEffect);
+
 	      
-		
-		/*
-		NoCardArea x = null;
-        try {
-	         FileInputStream fileIn = new FileInputStream("C://outout//ProductionAreaBig.ser");
-	         ObjectInputStream in = new ObjectInputStream(fileIn);
-	         x = (NoCardArea) in.readObject();
-	         in.close();
-	         fileIn.close();
-	      }catch(IOException i) {
-	         i.printStackTrace();
-	         return;
-	      }catch(Exception ex) {
-	         System.out.println("Employee class not found");
-	         ex.printStackTrace();
-	         return;
-	      }
-        System.out.println(x.getMaxOccupants());
-        */
+        Writer writer = new FileWriter(path);
+
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Effect.class,
+                        new PropertyBasedInterfaceMarshal()).create();
+        gson.toJson(area, writer);
+
+        writer.close();
+        
+        
+        
+        
+        NoCardArea aaa = gson.fromJson(new FileReader(path), NoCardArea.class);
+        System.out.println(aaa.getCost());
+
+
 	}
 }
