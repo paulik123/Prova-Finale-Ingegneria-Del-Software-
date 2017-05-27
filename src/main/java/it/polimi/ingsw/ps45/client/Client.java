806212afacd2f2549ba00e9e1801b924e423eaa9
Ene Paul <paulalexandru.ene@mail.polimi.it>
2017-05-27 16:38:44 +0200

    forgor to save

--- src/main/java/it/polimi/ingsw/ps45/client/Client.java
+++ src/main/java/it/polimi/ingsw/ps45/client/Client.java
@@ -42,15 +42,7 @@ public class Client {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-       
-        
-        
-        
-        
-        
-       
-        
+
         /*
         Scanner scanner = new Scanner(System.in);
 
